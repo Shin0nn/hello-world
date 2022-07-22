f610d824d7e56f9a3682ad237d5bebaf63592f70
Shin0nn <109781490+Shin0nn@users.noreply.github.com>
2022-07-22 14:57:48 +0900

    Update settings.js

--- github_oauth_sample-main/github_oauth_sample-main/settings.js
+++ github_oauth_sample-main/github_oauth_sample-main/settings.js
@@ -1,5 +1,5 @@
 //. settings.js
-exports.client_id = '';
-exports.client_secret = '';
-exports.callback_url = '';
-exports.repo_name = 'dotnsf/my_githubapi_test';
+exports.client_id = 'xxxxxxx'; OAuth App 作成時に取得した client_id の値
+exports.client_secret = 'xxxxxxx'; OAuth App 作成時に取得した client_secret の値
+exports.callback_url = 'http://localhost:8080/api/callback'; OAuth App 作成時に指定したコールバック URL の値
+exports.repo_name = 'dotnsf/my_githubapi_test';  操作対象リポジトリ名
